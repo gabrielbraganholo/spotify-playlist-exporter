9acff678179348031db6968a53d896e3affd3a42
Gabriel Morais Braganholo <gabrielmbraganholo@gmail.com>
2026-05-16 14:22:27 -0300

    SPE v-2.3

--- src/spotify.py
+++ src/spotify.py
@@ -8,14 +8,8 @@ class SpotifyClient:
 
     def __init__(self, token):
 
-        # Salva o token recebido após login OAuth
         self.token = token
-
-        # URL base da API do Spotify
         self.base_url = "https://api.spotify.com/v1"
-
-        # Headers enviados em TODAS as requisições
-        # Bearer = tipo de autenticação OAuth
         self.headers = {
             "Authorization": f"Bearer {self.token}"
         }
@@ -31,27 +25,14 @@ class SpotifyClient:
             list: Lista contendo informações das músicas
         """
 
-        # NOVO endpoint da API Spotify
-        # Antes era /tracks
-        # Agora a Spotify mudou para /items
         url = f"{self.base_url}/playlists/{playlist_id}/items"
 
-        # Lista onde iremos salvar todas as músicas
         tracks = []
 
-        # Continua executando enquanto existir próxima página
         while url:
 
-            # Faz requisição GET para a API
-            response = requests.get(
-                url,
-                headers=self.headers
-            )
+            response = requests.get(url, headers=self.headers)
 
-            # Debug para mostrar status da resposta
-            print(response.status_code)
-
-            # Se der erro, interrompe programa
             if response.status_code != 200:
 
                 print(response.text)
@@ -60,42 +41,20 @@ class SpotifyClient:
                     f"Erro ao buscar playlist: {response.text}"
                 )
 
-            # Converte JSON da resposta em dicionário Python
             data = response.json()
 
-            # Percorre cada item da playlist
             for item in data["items"]:
-
-                # Pega informações da música
                 track = item["item"]
 
-                # Algumas playlists podem ter itens vazios
                 if track is None:
                     continue
 
-                # Adiciona música na lista final
                 tracks.append({
-
-                    # Nome da música
                     "titulo": track["name"],
-
-                    # Junta todos artistas em uma string
-                    "artista": ", ".join(
-                        [
-                            artist["name"]
-                            for artist in track["artists"]
-                        ]
-                    ),
-
-                    # Data em que música foi adicionada
+                    "artista": ", ".join([artist["name"]for artist in track["artists"]]),
                     "data_adicao": item["added_at"]
                 })
 
-            # Próxima página da API
-            # Se não existir próxima página:
-            # next = None
-            # e o while encerra
             url = data["next"]
 
-        # Retorna lista final de músicas
         return tracks
